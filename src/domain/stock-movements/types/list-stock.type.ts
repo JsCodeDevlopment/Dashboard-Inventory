8f1ce1aa87a5e730dec67004bce31abf0d4fee9f
pedro <joaopsj007@gmail.com>
2025-03-14 10:56:12 -0300

    refactor: move stock-movement type

--- src/domain/stock-movements/types/list-stock.type.ts
+++ src/domain/stock-movements/types/list-stock.type.ts
@@ -1,23 +1,3 @@
-import { Product } from "@/domain/products/types/list-products.type";
-
-export interface StockMovement {
-  id: string;
-  productId: string;
-  productName: String;
-  quantity: number;
-  movementType: StockMovementType;
-  date: string;
-  product: Product;
-  serviceId?: string;
-  negotiatedValue: number;
-}
-
-export type StockMovementType = keyof typeof StockMovementType;
-export const StockMovementType = {
-  ENTRY: "ENTRY",
-  EXIT: "EXIT",
-} as const;
-
 export interface ListStockMovementProps {
   productName?: string;
   movementType?: string;
